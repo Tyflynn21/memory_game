113a6ea12226314eac81704b25a4b38eaeeef207
Ty Hart <tyhart028@yahoo.com>
2019-09-02 12:33:50 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -6,6 +6,14 @@ const cardsInPlay = []
 var cardOne = cards["1"];
 cardsInPlay.push('cardOne');
 
+var cardTwo = cards["2"];
+cardsInPlay.push('cardTwo');
+
+var cardThree = cards["3"];
+cardsInPlay.push('cardThree');
+
+var cardFour = cards["4"];
+cardsInPlay.push('cardFour');
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
